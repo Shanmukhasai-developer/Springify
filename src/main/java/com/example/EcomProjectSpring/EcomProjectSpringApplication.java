0d8200908fc039cbdbf5c39ace33322fd8d22133
Shanmukhasai-developer <kaparapushanmukhasai3@gmail.com>
2025-03-22 23:32:22 +0530

    first commit

---
+++ src/main/java/com/example/EcomProjectSpring/EcomProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcomProjectSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomProjectSpringApplication.class, args);
+	}
+
+}
